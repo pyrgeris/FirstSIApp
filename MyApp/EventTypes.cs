0afdd253f90bda3d08448c62a0eb937945f55a04
pyrgeris <pyrgeris@gmail.com>
2013-07-15 20:44:45 +0300

    used CSV adapter. timestamp taken from input file
    added input with timestamp + used microsoft's CSV input/output adapter .
    Pass Through query added + HopCount

--- MyApp/EventTypes.cs
+++ MyApp/EventTypes.cs
@@ -4,55 +4,25 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Globalization;
+using System.Diagnostics;
+using System.Threading;
+
 namespace MyApp
 {
-    public class newSensorReading
+   
+    public class MyReading
     {
-        public double input1 { get; set; }
-        public double input2 { get; set; }
-        public double input3 { get; set; }
-        public double input4 { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public string input1 { get; set; }
+        public string input2 { get; set; }
+        public string input3 { get; set; }
+        public string input4 { get; set; }
+        public string input5 { get; set; }
+    }
 
-        public override string ToString()
-        {
-            return new { input1, input2, input3, input4 }.ToString();
-        }
+    public class HopCount
+    {
+        public double Count { get; set; }
+    }
 
 
-        public static IEnumerable<newSensorReading> getSensorReadings(string filename)
-        {
-            CultureInfo _culture = new CultureInfo("en-US");
-
-            List<newSensorReading> events = new List<newSensorReading>();
-
-            string[] allLines = File.ReadAllLines(filename);
-
-            int count = 0;
-
-            foreach (var s in allLines)
-            {
-                if (s.Equals(""))
-                {
-                    Console.WriteLine("Blank string");
-                }
-                String[] data = s.Split(',');
-
-                newSensorReading e = new newSensorReading();
-
-                e.StartTime = DateTime.ParseExact("19891220 14:00:00", "yyyyMMdd HH:mm:ss", DateTimeFormatInfo.InvariantInfo).AddMilliseconds(200 * count).ToUniversalTime();
-                e.EndTime = e.StartTime;
-                e.input1 = double.Parse(data[0], System.Globalization.CultureInfo.InvariantCulture);
-                e.input2 = double.Parse(data[1], System.Globalization.CultureInfo.InvariantCulture);
-                e.input3 = double.Parse(data[2], System.Globalization.CultureInfo.InvariantCulture);
-                e.input4 = double.Parse(data[3], System.Globalization.CultureInfo.InvariantCulture);
-
-                count++;
-
-                events.Add(e);
-            }
-            return events;
-        }
-    }   
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -11,295 +11,160 @@ namespace MyApp
     using Microsoft.ComplexEventProcessing.Linq;
     using System.Collections.Generic;
     using Microsoft.ComplexEventProcessing.Extensibility;
+    using StreamInsight.Samples.Adapters.AsyncCsv;
+    using System.Diagnostics;
 
-    static class MyApp
+
+    public class MyApp
     {
-        [DisplayName("Pass-through")]
-        [Description("Pass-through query to just show input stream in the same form as we show output.")]
-        static void PassThrough(Application app)
+
+        private static int AskUserWhichQuery()
         {
+            Console.WriteLine();
+            Console.WriteLine("Pick a query:");
+            Console.WriteLine("0. Pass-through");
+            Console.WriteLine("1. Hop-Count");
+            Console.WriteLine("2. Exit");
 
-            var inputStream = GetNewSensorStream(app);
-            var query1 = from e in inputStream
-                         select e;
-            app.DisplayIntervalResults(query1);
+            string queryNumStr = Console.ReadLine();
+            int queryNum;
 
-        }
-
-
-        [DisplayName("Hopping Count")]
-        [Description("Report the count of input values being processed at some time over " +
-                     "a 10000 ms window, with the window moving in 200 ms hops. " +
-                     "Provide the counts as of the last reported result as of a point " +
-                     "in time, reflecting the input values processed over the last 10000 ms.")]
-        static void HoppingCount(Application app)
-        {
-            var inputStream = app.GetNewSensorStream();
-            var countStream = from win in inputStream.HoppingWindow(TimeSpan.FromMilliseconds(10000), TimeSpan.FromMilliseconds(200))
-                              select win.Count();
-            var query2 = countStream.ToPointEventStream();
-            app.DisplayPointResults(query2);
-        }
-
-
-        [DisplayName("Correlation Factor Hop Window")]
-        [Description("Report the count of input values being processed at some time over \n" +
-                     "a 10000 ms window, with the window moving in 200 ms hops. \n" +
-                     "Provide the counts as of the last reported result as of a point \n" +
-                     "in time, reflecting the input values processed over the last 10000 ms.\n")]
-        static void CorrelationFactor(Application app)
-        {
-            var inputStream = app.GetNewSensorStream();
-            var countStream = from win in inputStream.HoppingWindow(TimeSpan.FromMilliseconds(10000), TimeSpan.FromMilliseconds(200))
-                              select win.UserDefinedAggregate<newSensorReading, Correlation, double>(null);
-            var query3 = countStream.ToPointEventStream();
-            app.DisplayPointResults(query3);
-
-        }
-
-        [DisplayName("Correlation Factor Tumbling Window")]
-        [Description("Report the Correlation Factor of input1 and input2 being processed at some time over \n" +
-                     "a 10000 ms window. \n")]
-        static void CorrelationFactor3(Application app)
-        {
-            var inputStream = app.GetNewSensorStream();
-            var countStream = from win in inputStream.TumblingWindow(TimeSpan.FromMilliseconds(10000))
-                              select win.UserDefinedAggregate<newSensorReading, Correlation, double>(null);
-            var query4 = countStream.ToPointEventStream();
-            app.DisplayPointResults(query4);
-
-        }
-
-
-        static IQStreamable<newSensorReading> GetNewSensorStream(this Application app)
-        {
-            //AdvanceTimeGenerationSettings atgs =  new AdvanceTimeGenerationSettings(1,-TimeSpan.FromMilliseconds(100));
-            //AdvanceTimeSettings ats = new AdvanceTimeSettings(atgs,null,AdvanceTimePolicy.Drop);
-            IEnumerable<newSensorReading> newsensorReadings = newSensorReading.getSensorReadings(newSensorReadingsFileName);
-            var newsensorStream = app.DefineEnumerable<newSensorReading>(() => newsensorReadings).ToPointStreamable<newSensorReading, newSensorReading>(
-                        e => PointEvent.CreateInsert<newSensorReading>(e.StartTime, e),
-                        AdvanceTimeSettings.IncreasingStartTime);
-            return newsensorStream;
-        }
-
-        public class Correlation : CepAggregate<newSensorReading, double>
-        {
-            public override double GenerateOutput(IEnumerable<newSensorReading> newsensorStream)
+            try
             {
-                List<double> items = new List<double>();
-                List<double> items2 = new List<double>();
-                foreach (var e in newsensorStream) { items.Add(e.input3); items2.Add(e.input2); }
-
-                double[] array1 = items.ToArray();
-                double[] array2 = items2.ToArray();
-
-                double[] array_xy = new double[array1.Length];
-                double[] array_xp2 = new double[array1.Length];
-                double[] array_yp2 = new double[array1.Length];
-                for (int i = 0; i < array1.Length; i++)
-                    array_xy[i] = array1[i] * array2[i];
-                for (int i = 0; i < array1.Length; i++)
-                    array_xp2[i] = Math.Pow(array1[i], 2.0);
-                for (int i = 0; i < array1.Length; i++)
-                    array_yp2[i] = Math.Pow(array2[i], 2.0);
-                double sum_x = 0;
-                double sum_y = 0;
-                foreach (double n in array1)
-                    sum_x += n;
-                foreach (double n in array2)
-                    sum_y += n;
-                double sum_xy = 0;
-                foreach (double n in array_xy)
-                    sum_xy += n;
-                double sum_xpow2 = 0;
-                foreach (double n in array_xp2)
-                    sum_xpow2 += n;
-                double sum_ypow2 = 0;
-                foreach (double n in array_yp2)
-                    sum_ypow2 += n;
-                double Ex2 = Math.Pow(sum_x, 2.00);
-                double Ey2 = Math.Pow(sum_y, 2.00);
-
-                double Correl = (array1.Length * sum_xy - sum_x * sum_y) / Math.Sqrt((array1.Length * sum_xpow2 - Ex2) * (array1.Length * sum_ypow2 - Ey2));
-
-                return Correl;
-
+                queryNum = Int32.Parse(queryNumStr, new CultureInfo("en-US"));
+            }
+            catch (FormatException)
+            {
+                queryNum = 2;
             }
 
+            return queryNum;
+        }
+
+        static void PassThrough()
+        {
+            var inputStream = CepStream<MyReading>.Create("MyStream");
+            // Define input stream object, mapped to stream names
+            // Instantiate an adapter from the input factory class
+            var queryStream = from e in inputStream select e;
+            BindAndRunQuery(
+                "PassThrough",
+                queryStream,
+                EventShape.Interval,
+                new List<string>() { "input1", "input2", "input3", "input4" , "input5"},
+                new List<string>() { "input1", "input2", "input3", "input4", "input5" });
+
+        }
+
+        static void HopCount()
+        {
+            var inputStream = CepStream<MyReading>.Create("MyStream");
+            // Define input stream object, mapped to stream names
+            // Instantiate an adapter from the input factory class
+            var queryStream = from w in inputStream.HoppingWindow(TimeSpan.FromMilliseconds(20000),    // Window Size
+                                            TimeSpan.FromMilliseconds(10 * 200),    // Hop Size
+                                            HoppingWindowOutputPolicy.ClipToWindowEnd)
+                              select new HopCount { Count = w.Count() };
+            BindAndRunQuery(
+                "HopCount",
+                queryStream,
+                EventShape.Interval,
+                new List<string>() { "input1", "input2", "input3", "input4", "input5" },
+                new List<string>() { "Count" });
+
         }
 
 
-        #region Program plumbing
-
-        static void DisplayPointResults<TPayload>(this Application app, IQStreamable<TPayload> resultStream)
+        private static void BindAndRunQuery<TPayload>(string queryName, CepStream<TPayload> queryStream, EventShape outputEventShape, List<string> inputFields, List<string> outputFields)
         {
-            // Define observer that formats arriving events as points to the console window.
-            var consoleObserver = app.DefineObserver(() => Observer.Create<PointEvent<TPayload>>(ConsoleWritePoint));
-
-            // Bind resultStream stream to consoleObserver.
-            var binding = resultStream.Bind(consoleObserver);
-
-            // Run example by creating a process from the binding we built above.
-            using (binding.Run("ExampleProcess"))
+            var inputConfig = new CsvInputConfig
             {
-                Console.WriteLine("***Hit Return to exit after viewing query output***");
-                Console.WriteLine();
-                Console.ReadLine();
+                InputFileName = @"..\MyApp\Input Data\input.csv",
+                Delimiter = new char[] { ',' },
+                BufferSize = 4096,
+                CtiFrequency = 1,
+                CultureName = "en-US",
+                Fields = inputFields,
+                NonPayloadFieldCount = 0,
+                StartTimePos = 1,
+                EndTimePos = 6
+            };
+
+            // The adapter recognizes empty filename as a write to console.
+            var outputConfig = new CsvOutputConfig
+            {
+                OutputFileName = @"..\MyApp\Output Data\output.txt",
+                Delimiter = new string[] { "\t" },
+                CultureName = "en-US",
+                Fields = outputFields
+            };
+
+            // Note - Please change the instance name to the one you have chosen during installation
+            using (var server = Server.Create("Instance1"))
+            {
+                Application application = server.CreateApplication("MyApp");
+
+                // set up input and output adapters
+                InputAdapter inputAdapter = application.CreateInputAdapter<CsvInputFactory>("input", "Csv Input Source");
+                OutputAdapter outputAdapter = application.CreateOutputAdapter<CsvOutputFactory>("output", "Csv Output");
+
+                // set up the query template
+                QueryTemplate queryTemplate = application.CreateQueryTemplate("QueryTemplate", string.Empty, queryStream);
+
+                // set up advance time settings to enqueue CTIs
+                var advanceTimeGenerationSettings = new AdvanceTimeGenerationSettings(inputConfig.CtiFrequency, TimeSpan.FromMilliseconds(200), true);
+                var advanceTimeSettings = new AdvanceTimeSettings(advanceTimeGenerationSettings, null, AdvanceTimePolicy.Adjust);
+
+                // Bind query template to input and output streams
+                QueryBinder queryBinder = new QueryBinder(queryTemplate);
+                queryBinder.BindProducer<MyReading>("MyStream", inputAdapter, inputConfig, EventShape.Point, advanceTimeSettings);
+                queryBinder.AddConsumer("outputStream", outputAdapter, outputConfig, outputEventShape, StreamEventOrder.FullyOrdered);
+
+                // Create a runnable query by binding the query template to the input stream of interval events,
+                // and to an output stream of fully ordered point events (through an output adapter instantiated
+                // from the output factory class)
+                Query query = application.CreateQuery(queryName, "Query", queryBinder);
+
+                RunQuery(query);
             }
         }
 
-        static void ConsoleWritePoint<TPayload>(PointEvent<TPayload> e)
+        private static void RunQuery(Query query)
         {
-            if (e.EventKind == EventKind.Insert)
-                WriteLine(string.Format(CultureInfo.InvariantCulture, "INSERT <{0}> {1}", e.StartTime.DateTime, e.Payload.ToString()));
-            else
-                WriteLine(string.Format(CultureInfo.InvariantCulture, "CTI    <{0}>", e.StartTime.DateTime));
+            query.Start();
+
+            Console.WriteLine("***Hit Return to exit after viewing query output***");
+            Console.WriteLine();
+            Console.ReadLine();
+
+            query.Stop();
         }
 
-        static void DisplayIntervalResults<TPayload>(this Application app, IQStreamable<TPayload> resultStream)
+
+        internal static void Main()
         {
-            // Define observer that formats arriving events as intervals to the console window.
-            var consoleObserver = app.DefineObserver(() => Observer.Create<IntervalEvent<TPayload>>(ConsoleWriteInterval));
-
-            // Bind resultStream stream to consoleObserver.
-            var binding = resultStream.Bind(consoleObserver);
-
-            // Run example query by creating a process from the binding we've built above.
-            using (binding.Run("ExampleProcess"))
+            while (true)
             {
-                Console.WriteLine("***Hit Return to exit after viewing query output***");
-                Console.WriteLine();
-                Console.ReadLine();
-            }
-        }
+                int queryToRun = AskUserWhichQuery();
 
-        static void ConsoleWriteInterval<TPayload>(IntervalEvent<TPayload> e)
-        {
-            if (e.EventKind == EventKind.Insert)
-                WriteLine(string.Format(CultureInfo.InvariantCulture, "INSERT <{0} - {1}> {2}", e.StartTime.DateTime, e.EndTime.DateTime, e.Payload.ToString()));
-            else
-                WriteLine(string.Format(CultureInfo.InvariantCulture, "CTI    <{0}>", e.StartTime.DateTime));
-        }
-
-        static void DisplayEdgeResults<TPayload>(this Application app, IQStreamable<TPayload> resultStream)
-        {
-            // Define observer that formats arriving events as intervals to the console window.
-            var consoleObserver = app.DefineObserver(() => Observer.Create<EdgeEvent<TPayload>>(ConsoleWriteEdge));
-
-            // Bind resultStream stream to consoleObserver.
-            var binding = resultStream.Bind(consoleObserver);
-
-            // Run example query by creating a process from the binding we've built above.
-            using (binding.Run("ExampleProcess"))
-            {
-                Console.WriteLine("***Hit Return to exit after viewing query output***");
-                Console.WriteLine();
-                Console.ReadLine();
-            }
-        }
-
-        static void ConsoleWriteEdge<TPayload>(EdgeEvent<TPayload> e)
-        {
-            if (e.EventKind == EventKind.Insert)
-            {
-                WriteLine(string.Format(CultureInfo.InvariantCulture, "START <{0} - {1}> {2}", e.StartTime.DateTime, "Infinitive", e.Payload.ToString()));
-            }
-            else
-                WriteLine(string.Format(CultureInfo.InvariantCulture, "CTI    <{0}>", e.StartTime.DateTime));
-        }
-
-        private static void WriteLine(String line)
-        {
-            Console.WriteLine(line);
-            Console.WriteLine();//add one more empty line to for clearer display
-
-            writeToOutputFile(line);
-        }
-
-        private static void WriteLine()
-        {
-            WriteLine("");
-        }
-
-
-        private static string newSensorReadingsFileName = @"Input Data\input.csv";
-
-        private static string OutputFileName = @"output.txt";
-
-        private static void prepareOutputFile()
-        {
-            System.IO.StreamWriter file = new System.IO.StreamWriter(OutputFileName);
-            file.WriteLine("------RESULT-----");
-            file.Close();
-        }
-
-        private static void writeToOutputFile(String line)
-        {
-            System.IO.StreamWriter file = new System.IO.StreamWriter(OutputFileName, true);
-            file.WriteLine(line);
-            file.Close();
-        }
-
-        static void Main()
-        {
-            Console.WriteLine("Output data will be displayed on console and also persited to file " + OutputFileName + ".\n");
-
-            using (var server = Server.Create("instance1"))
-            {
-
-                Application application = server.CreateApplication("AssignmentApp");
-
-                // collect all supported queries as defined in the above sections of this code
-                var demos = (from mi in typeof(MyApp).GetMethods(BindingFlags.Static | BindingFlags.NonPublic)
-                             let nameAttr = mi.GetCustomAttributes(typeof(DisplayNameAttribute), false)
-                                 .OfType<DisplayNameAttribute>()
-                                 .SingleOrDefault()
-                             let descriptionAttr = mi.GetCustomAttributes(typeof(DescriptionAttribute), false)
-                                 .OfType<DescriptionAttribute>()
-                                 .SingleOrDefault()
-                             where null != nameAttr
-                             select new { Action = mi, Name = nameAttr.DisplayName, Description = descriptionAttr.Description }).ToArray();
-
-                prepareOutputFile();
-
-                // repeated ask user for a query type to run
-                while (true)
+                switch (queryToRun)
                 {
-                    Console.WriteLine();
-                    Console.WriteLine("Pick a query type:");
-                    for (int demo = 0; demo < demos.Length; demo++)
-                    {
-                        Console.WriteLine("{0,4} - {1}", demo, demos[demo].Name);
-                    }
-
-                    Console.WriteLine("Exit - Exit from Demo.");
-                    var response = Console.ReadLine().Trim();
-                    if (string.Equals(response, "exit", StringComparison.OrdinalIgnoreCase) ||
-                        string.Equals(response, "e", StringComparison.OrdinalIgnoreCase))
-                    {
+                    case 0:
+                        PassThrough();
                         break;
-                    }
-
-                    int demoToRun;
-                    demoToRun = Int32.TryParse(response, NumberStyles.Integer, CultureInfo.InvariantCulture, out demoToRun)
-                        ? demoToRun
-                        : -1;
-
-                    if (0 <= demoToRun && demoToRun < demos.Length)
-                    {
-                        WriteLine();
-                        WriteLine(demos[demoToRun].Name);
-                        WriteLine(demos[demoToRun].Description);
-                        // run the query
-                        demos[demoToRun].Action.Invoke(null, new[] { application });
-                    }
-                    else
-                    {
+                    case 1:
+                        HopCount();
+                        break;
+                    case 12:
+                        return;
+                    default:
                         Console.WriteLine("Unknown Query Demo");
-                    }
+                        break;
                 }
             }
         }
-        #endregion
+
+
+
     }
 }
